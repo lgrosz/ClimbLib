8ae9c8e43f71daed84d9145e0bd95c69e89b909c
Logan Grosz <logan.grosz@gmail.com>
2024-05-08 21:11:36 -0600

    Adds homebrewed verify

--- tests.c
+++ tests.c
@@ -1,29 +1,37 @@
-#include <assert.h>
+#include <stdio.h>
 #include <errno.h>
 #include <stdlib.h>
 #include "grades.h"
+
+#define VERIFY(expr) \
+    do { \
+        if (!(expr)) { \
+            fprintf(stderr, "Verification failed at %s:%d - %s\n", __FILE__, __LINE__, #expr); \
+            exit(EXIT_FAILURE); \
+        } \
+    } while (0)
 
 int main()
 {
 	// Test free function
 	errno = 0;
 	GradeHueco_free(NULL);
-	assert(errno == EINVAL);
+	VERIFY(errno == EINVAL);
 
 	// Test new function
 	GradeHueco a = GradeHueco_new(0, GRADE_HUECO_MODIFIER_NONE);
-	assert(a != NULL);
+	VERIFY(a != NULL);
 	GradeHueco_free(a);
 
 	// Test duplicate function
 	GradeHueco b = GradeHueco_dup(a);
-	assert(b != NULL);
+	VERIFY(b != NULL);
 	GradeHueco_free(b);
 
 	errno = 0;
 	b = GradeHueco_dup(NULL);
-	assert(b == NULL);
-	assert(errno == EINVAL);
+	VERIFY(b == NULL);
+	VERIFY(errno == EINVAL);
 
 	return 0;
 }
