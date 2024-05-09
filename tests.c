7e71a673ca1098475ab4119e51295c4270212a04
Logan Grosz <logan.grosz@gmail.com>
2024-05-08 21:08:17 -0600

    Fixup tests

--- tests.c
+++ tests.c
@@ -1,17 +1,29 @@
 #include <assert.h>
+#include <errno.h>
 #include <stdlib.h>
 #include "grades.h"
 
 int main()
 {
+	// Test free function
+	errno = 0;
+	GradeHueco_free(NULL);
+	assert(errno == EINVAL);
+
+	// Test new function
 	GradeHueco a = GradeHueco_new(0, GRADE_HUECO_MODIFIER_NONE);
 	assert(a != NULL);
+	GradeHueco_free(a);
 
+	// Test duplicate function
 	GradeHueco b = GradeHueco_dup(a);
 	assert(b != NULL);
+	GradeHueco_free(b);
 
-	free(a);
-	free(b);
+	errno = 0;
+	b = GradeHueco_dup(NULL);
+	assert(b == NULL);
+	assert(errno == EINVAL);
 
 	return 0;
 }
