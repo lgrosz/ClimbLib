a694a6fed1394bc5690a9def54ee81dec7c314b1
Logan Grosz <logan.grosz@gmail.com>
2024-05-15 23:10:15 -0600

    Adds Climb_name and Climb_set_name

--- climb.c
+++ climb.c
@@ -1,9 +1,11 @@
 #include "climb.h"
 #include <errno.h>
 #include <stdlib.h>
+#include <string.h>
 
 struct Climb_
 {
+	const char *name;
 };
 
 Climb Climb_new()
@@ -14,6 +16,16 @@ Climb Climb_new()
 		errno = ENOMEM;
 		return NULL;
 	}
+
+	char *name;
+	if (NULL == (name = malloc(strlen("")+1))) {
+		errno = ENOMEM;
+		free(ret);
+		return NULL;
+	}
+
+	strcpy(name, "");
+	ret->name = name;
 
 	errno = 0;
 	return ret;
@@ -27,5 +39,28 @@ void Climb_free(Climb climb)
 	}
 
 	errno = 0;
+	free((void*)climb->name);
 	free(climb);
+}
+
+void Climb_set_name(Climb climb, const char *name)
+{
+	errno = 0;
+	if (climb->name) {
+		free((void*)climb->name);
+	}
+
+	char *result;
+	if (NULL == (result = malloc(strlen(name)+1))) {
+		errno = ENOMEM;
+		return;
+	}
+
+	strcpy(result, name);
+	climb->name = result;
+}
+
+const char *Climb_name(Climb climb)
+{
+	return climb->name;
 }
--- tests/climb.c
+++ tests/climb.c
@@ -1,6 +1,7 @@
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "verify.h"
 
@@ -22,10 +23,32 @@ static void test_new()
 	VERIFY(errno == 0);
 }
 
+static void test_name()
+{
+	Climb c = Climb_new();
+	VERIFY(errno == 0);
+	VERIFY(c != NULL);
+
+	VERIFY(strcmp(Climb_name(c), "") == 0);
+
+	char name[] = "A Problem Name";
+	Climb_set_name(c, name);
+	VERIFY(errno == 0);
+	VERIFY(strcmp(Climb_name(c), name) == 0);
+
+	Climb_set_name(c, "");
+	VERIFY(errno == 0);
+	VERIFY(strcmp(Climb_name(c), "") == 0);
+
+	Climb_free(c);
+	VERIFY(errno == 0);
+}
+
 int climb()
 {
 	test_free_null();
 	test_new();
+	test_name();
 
 	exit(EXIT_SUCCESS);
 }
