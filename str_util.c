817c931c2476769443cb8adc2aa161b273a9d56a
Logan Grosz <logan.grosz@gmail.com>
2024-06-02 22:04:12 -0600

    Explicitly multiplies allocations by sizeof char
    Even though the c-standard assures `sizeof(char) == 1`, it seems like
    good practice to explicitly do this.

--- str_util.c
+++ str_util.c
@@ -8,7 +8,7 @@ void free_allocate_and_assign_str(char const **str_addr, const char *str)
 {
 	errno = 0;
 
-	*str_addr = realloc((void*)*str_addr, strlen(str) + 1);
+	*str_addr = realloc((void*)*str_addr, sizeof(char) * (strlen(str) + 1));
 
 	if (*str_addr != NULL) {
 		strcpy((char*)*str_addr, str);
