289c24a1cd4d1920ffd95b63732b9c7482b8c06a
Logan Grosz <logan.grosz@gmail.com>
2024-06-09 12:26:30 -0600

    Adds "variation" edges

--- graph.c
+++ graph.c
@@ -2,9 +2,17 @@
 #include <errno.h>
 #include <stdlib.h>
 
+typedef struct VariationEdge_ {
+	ClimbNode c;
+	ClimbNode v;
+} VariationEdge_;
+
 struct ClimbGraph_ {
 	ClimbNode *climbs;
 	size_t climbslen;
+
+	VariationEdge_ *variations;
+	size_t variationslen;
 };
 
 ClimbGraph ClimbGraph_new()
@@ -16,6 +24,8 @@ ClimbGraph ClimbGraph_new()
 	} else {
 		ret->climbs = NULL;
 		ret->climbslen = 0;
+		ret->variations = NULL;
+		ret->variationslen = 0;
 	}
 
 	errno = 0;
@@ -31,6 +41,10 @@ void ClimbGraph_free(ClimbGraph graph)
 
 	if (NULL != graph->climbs) {
 		free(graph->climbs);
+	}
+
+	if (NULL != graph->variations) {
+		free(graph->variations);
 	}
 
 	free(graph);
@@ -92,6 +106,76 @@ int ClimbGraph_has_climb(ClimbGraph graph, ClimbNode node)
 
 	for (size_t i = 0; i < graph->climbslen; i++) {
 		if (graph->climbs[i] == node) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
+
+void ClimbGraph_add_variation(ClimbGraph g, const ClimbNode c, const ClimbNode v)
+{
+	if (g == NULL || c == NULL || v == NULL) {
+		errno = EINVAL;
+		return;
+	}
+
+	if (!ClimbGraph_has_climb(g, c) || !ClimbGraph_has_climb(g, v)) {
+		errno = EINVAL;
+		return;
+	}
+
+	if (NULL == (g->variations = realloc(g->variations, sizeof(VariationEdge_) * (g->variationslen + 1)))) {
+		return;
+	}
+
+	VariationEdge_ edge = { c, v };
+
+	g->variations[g->variationslen] = edge;
+	g->variationslen = g->variationslen + 1;
+	errno = 0;
+}
+
+void ClimbGraph_remove_variation(ClimbGraph g, const ClimbNode c, const ClimbNode v)
+{
+	if (g == NULL || c == NULL || v == NULL) {
+		errno = EINVAL;
+		return;
+	}
+
+	int index = -1;
+
+	for (int i = 0; i < g->variationslen; i++) {
+		if (g->variations[i].c == c && g->variations[i].v == v) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index == -1) {
+		return;
+	}
+
+	for (int i = index; i < g->variationslen - 1; i++) {
+		g->variations[i] = g->variations[i + 1];
+	}
+
+	if (NULL == (g->variations = realloc(g->variations, sizeof(ClimbNode) * (g->variationslen - 1)))) {
+		return;
+	}
+
+	g->variationslen = g->variationslen - 1;
+}
+
+int ClimbGraph_has_variation(const ClimbGraph g, const ClimbNode c, const ClimbNode v)
+{
+	if (g == NULL || c == NULL || v == NULL) {
+		errno = EINVAL;
+		return 0;
+	}
+
+	for (int i = 0; i < g->variationslen; i++) {
+		if (g->variations[i].c == c && g->variations[i].v == v) {
 			return 1;
 		}
 	}
