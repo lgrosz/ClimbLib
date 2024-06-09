6d95d67f173f7df74effbd6bb91e93395c42b29e
Logan Grosz <logan.grosz@gmail.com>
2024-06-09 10:56:39 -0600

    Adds "climb" vertex

--- graph.c
+++ graph.c
@@ -36,3 +36,65 @@ void ClimbGraph_free(ClimbGraph graph)
 	free(graph);
 	errno = 0;
 }
+
+void ClimbGraph_add_climb(ClimbGraph graph, const ClimbNode node) {
+	if (graph == NULL || node == NULL) {
+		errno = EINVAL;
+		return;
+	}
+
+	if (NULL == (graph->climbs = realloc(graph->climbs, sizeof(ClimbNode) * (graph->climbslen + 1)))) {
+		return;
+	}
+
+	graph->climbs[graph->climbslen] = node;
+	graph->climbslen = graph->climbslen + 1;
+	errno = 0;
+}
+
+void ClimbGraph_remove_climb(ClimbGraph graph, const ClimbNode node)
+{
+	if (graph == NULL || node == NULL) {
+		errno = EINVAL;
+		return;
+	}
+
+	int index = -1;
+
+	for (int i = 0; i < graph->climbslen; i++) {
+		if (graph->climbs[i] == node) {
+			index = i;
+			break;
+		}
+	}
+
+	if (index == -1) {
+		return;
+	}
+
+	for (int i = index; i < graph->climbslen - 1; i++) {
+		graph->climbs[i] = graph->climbs[i + 1];
+	}
+
+	if (NULL == (graph->climbs = realloc(graph->climbs, sizeof(ClimbNode) * (graph->climbslen - 1)))) {
+		return;
+	}
+
+	graph->climbslen = graph->climbslen - 1;
+}
+
+int ClimbGraph_has_climb(ClimbGraph graph, ClimbNode node)
+{
+	if (graph == NULL || node == NULL) {
+		errno = EINVAL;
+		return 0;
+	}
+
+	for (size_t i = 0; i < graph->climbslen; i++) {
+		if (graph->climbs[i] == node) {
+			return 1;
+		}
+	}
+
+	return 0;
+}
