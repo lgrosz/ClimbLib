470c12f47d288d083070782e904c2d53298e5100
Logan Grosz <logan.grosz@gmail.com>
2024-06-18 00:06:01 -0600

    WIP Docs for graph.h

--- graph.h
+++ graph.h
@@ -1,15 +1,73 @@
 #ifndef _CLIMBLIB_GRAPH_H
 #define _CLIMBLIB_GRAPH_H
 
+/**
+ * @file graph.h
+ * @brief Header prototyping ClimbGraph_* methods
+ */
+
 #include "node.h"
 
+/**
+ * @class ClimbGraph
+ * @brief An referential type for a graph of climbs
+ *
+ * An opaque, referential, type that details climbs in a graph relationship.
+ */
 typedef struct ClimbGraph_* ClimbGraph;
 
+/**
+ * @brief Creates a new @ref ClimbGraph
+ * @memberof ClimbGraph
+ *
+ * @return A new @ref ClimbGraph
+ */
 ClimbGraph ClimbGraph_new();
+
+/**
+ * @brief Frees @p graph
+ * @memberof ClimbGraph
+ *
+ * @param graph The @ref ClimbGraph to free
+ */
 void ClimbGraph_free(ClimbGraph);
+
+/**
+ * @brief Adds an @p node to the @p graph.
+ * @memberof ClimbGraph
+ *
+ * @param graph The @ref ClimbGraph which to add @p node
+ * @param node The @ref ClimbNode which to add to @p graph
+ */
 void ClimbGraph_add_climb(ClimbGraph, const ClimbNode);
+
+/**
+ * @brief Removes @p node from @p graph
+ * @memberof ClimbGraph
+ *
+ * @param graph The @ref ClimbGraph which to remove @p node
+ * @param node The @ref ClimbNode to remove from @p graph
+ */
 void ClimbGraph_remove_climb(ClimbGraph, const ClimbNode);
+
+/**
+ * @brief Checks whether @p graph contains @p node
+ * @memberof ClimbGraph
+ *
+ * @param graph The @ref ClimbGraph to check
+ * @param node The @ref ClimbNode to check for
+ * @return `1` if @p graph contains @p node, `0` otherwise
+ */
 int ClimbGraph_has_climb(const ClimbGraph, const ClimbNode);
+
+/**
+ * @brief All the @ref ClimbNode ["(ClimbNode)"]s contained in @p graph
+ * @memberof ClimbGraph
+ *
+ * @param graph @ref ClimbGraph to read from
+ * @param size Address to load with the size of the returned array
+ * @return An array of size @p size
+ */
 ClimbNode *ClimbGraph_climbs(const ClimbGraph, size_t *);
 
 // Climb variations
--- node.h
+++ node.h
@@ -1,8 +1,17 @@
 #ifndef _CLIMBLIB_NODE_H_
 #define _CLIMBLIB_NODE_H_
 
+/**
+ * @file node.h
+ * @brief Header prototyping nodes
+ */
+
 #include "climb.h"
 
+/**
+ * @class ClimbNode
+ * @brief A node for @ref Climb [Climb]s
+ */
 typedef struct ClimbNode_* ClimbNode;
 
 ClimbNode ClimbNode_new(Climb);
