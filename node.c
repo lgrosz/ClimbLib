aa48f7dee6199a5a622180abac5dc5de7e1f6d49
Logan Grosz <logan.grosz@gmail.com>
2024-06-26 22:43:47 -0600

    Adds formation node

--- node.c
+++ node.c
@@ -6,6 +6,10 @@ struct ClimbNode_ {
 	Climb climb;
 };
 
+struct FormationNode_ {
+	Formation climb;
+};
+
 ClimbNode ClimbNode_new(Climb climb)
 {
 	ClimbNode ret;
@@ -32,6 +36,41 @@ void ClimbNode_free(ClimbNode node)
 }
 
 Climb ClimbNode_climb(const ClimbNode node)
+{
+	if (node == NULL) {
+		errno = EINVAL;
+		return NULL;
+	}
+
+	return node->climb;
+}
+
+FormationNode FormationNode_new(Formation climb)
+{
+	FormationNode ret;
+
+	if (NULL == (ret = malloc(sizeof(FormationNode)))) {
+		return NULL;
+	} else {
+		ret->climb = climb;
+	}
+
+	errno = 0;
+	return ret;
+}
+
+void FormationNode_free(FormationNode node)
+{
+	if (node == NULL) {
+		errno = EINVAL;
+		return;
+	}
+
+	free(node);
+	errno = 0;
+}
+
+Formation FormationNode_climb(const FormationNode node)
 {
 	if (node == NULL) {
 		errno = EINVAL;
--- node.h
+++ node.h
@@ -2,11 +2,17 @@
 #define _CLIMBLIB_NODE_H_
 
 #include "climb.h"
+#include "formation.h"
 
 typedef struct ClimbNode_* ClimbNode;
+typedef struct FormationNode_* FormationNode;
 
 ClimbNode ClimbNode_new(Climb);
 void ClimbNode_free(ClimbNode);
 Climb ClimbNode_climb(const ClimbNode);
+
+FormationNode FormationNode_new(Formation);
+void FormationNode_free(FormationNode);
+Formation FormationNode_climb(const FormationNode);
 
 #endif
