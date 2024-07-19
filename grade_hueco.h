32929f3473999e43ee03c8d91d76065803f6d316
Logan Grosz <logan.grosz@gmail.com>
2024-07-18 23:18:34 -0600

    Briefs all declarations in grade_hueco.h

--- grade_hueco.h
+++ grade_hueco.h
@@ -1,21 +1,63 @@
 #ifndef _CLIMBLIB_GRADE_HUECO_H_
 #define _CLIMBLIB_GRADE_HUECO_H_
 
+/**
+ * @file grade_hueco.h
+ *
+ * @brief Header that defines structures and methods for the Hueco grading
+ * system (V-scale).
+ */
+
 #include <stddef.h>
 
+/**
+ * @brief Modifiers that granulate difficulty within a grade
+ */
 typedef enum GradeHuecoModifier {
+	/**
+	 * @brief The "-" modifier
+	 */
 	GRADE_HUECO_MODIFIER_MINUS,
+
+	/**
+	 * @brief No modifier present
+	 */
 	GRADE_HUECO_MODIFIER_NONE,
+
+	/**
+	 * @brief The "+" modifier
+	 */
 	GRADE_HUECO_MODIFIER_PLUS,
 } GradeHuecoModifier;
 
+/**
+ * @brief A Hueco grade.
+ */
 typedef struct {
+	/**
+	 * @brief The value of the grade.
+	 */
 	unsigned int grade;
-	GradeHuecoModifier modifier;
+
+        /**
+         * @brief A modifier to refine the grade.
+         */
+        GradeHuecoModifier modifier;
 } GradeHueco;
 
+/**
+ * @brief Compares two @ref GradeHueco instances.
+ */
 int GradeHueco_cmp(const GradeHueco, const GradeHueco);
+
+/**
+ * @brief Produces a string representing the Hueco grade.
+ */
 int GradeHueco_str(const GradeHueco, char *, size_t);
+
+/**
+ * @brief Populates a @ref GradeHueco from a representative string.
+ */
 int GradeHueco_fromstr(const char *, GradeHueco *);
 
 #endif
