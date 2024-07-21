bb1add7def0bdc90a023107e129f4b8fd76c85e3
Logan Grosz <logan.grosz@gmail.com>
2024-07-21 12:09:02 -0600

    Adds node list to graph

--- tests/graph.c
+++ tests/graph.c
@@ -19,6 +19,38 @@ START_TEST(test_new)
 }
 END_TEST
 
+START_TEST(test_nodes)
+{
+	Graph *graph;
+	Node *a, *b, *c, *n;
+
+	graph = Graph_new();
+	a = Node_new();
+	b = Node_new();
+	c = Node_new();
+
+	ck_assert_ptr_null(Graph_get_nodes(NULL));
+	ck_assert_ptr_null(Graph_get_nodes(graph));
+	ck_assert_int_eq(!Graph_add_node(NULL, a), 0);
+	ck_assert_int_eq(!Graph_add_node(graph, NULL), 0);
+
+	ck_assert_int_eq(Graph_add_node(graph, a), 0);
+	ck_assert_int_eq(Graph_add_node(graph, b), 0);
+	ck_assert_int_eq(Graph_add_node(graph, c), 0);
+
+	ck_assert_ptr_eq(n = Graph_get_nodes(graph), a);
+	ck_assert_ptr_null(Node_get_next(NULL));
+	ck_assert_ptr_eq(n = Node_get_next(n), b);
+	ck_assert_ptr_eq(n = Node_get_next(n), c);
+	ck_assert_ptr_null(n = Node_get_next(n));
+
+	Graph_free(graph);
+	Node_free(a);
+	Node_free(b);
+	Node_free(c);
+}
+END_TEST
+
 static Suite *suite()
 {
 	Suite *s;
@@ -28,6 +60,7 @@ static Suite *suite()
 
 	tc_core = tcase_create("Core");
 	tcase_add_test(tc_core, test_new);
+	tcase_add_test(tc_core, test_nodes);
 
 	suite_add_tcase(s, tc_core);
 
