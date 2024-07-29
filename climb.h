4a00c57364a86a949e24f0cbf9657ffbcec6480d
Logan Grosz <logan.grosz@gmail.com>
2024-07-28 20:57:27 -0600

    Adds grades to climbs

--- climb.h
+++ climb.h
@@ -3,6 +3,7 @@
 
 #include <stddef.h>
 
+typedef struct GradeItem GradeItem;
 typedef struct Climb Climb;
 
 Climb *Climb_new();
@@ -20,5 +21,19 @@ const char *Climb_brief(const Climb *);
 void Climb_add_alias(Climb *, const char *);
 void Climb_remove_alias(Climb *, const char *);
 const char **Climb_aliases(const Climb *, size_t *);
+
+/**
+ * @brief Sets grades for climb
+ *
+ * @returns Previous grades for climb
+ */
+GradeItem *Climb_set_grades(Climb *, GradeItem *);
+
+/**
+ * @brief Grades for climb
+ *
+ * @returns Grades for climb
+ */
+GradeItem *Climb_grades(Climb *);
 
 #endif
